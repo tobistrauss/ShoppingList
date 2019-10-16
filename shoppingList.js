7b7bcc3f7bbfd022852ba1a8e1d2fab00f01a616
Tobias Strauss <54841030+tobistrauss@users.noreply.github.com>
2019-10-16 18:02:57 +0200

    Add files via upload

---
+++ shoppingList.js
@@ -0,0 +1,18 @@
+//returns the list
+{
+    "_id": "5d9c7b9ee2d16f00178a5c6b",
+    "userId": "<userid>",
+    "name": "Einkaufen gehen",
+    "items": [
+        {
+            "bought": true,
+            "_id": "<imteid>",
+            "name": "Bananen"
+        },
+        {
+            "bought": false,
+            "_id": "<imteid>",
+            "name": "Äpfel"
+        }
+    ]
+}   
