9a2d8a928c1019e77874884eb090831a346ef894
Tobias Strauss <54841030+tobistrauss@users.noreply.github.com>
2019-10-18 15:34:33 +0200

    Add files via upload

--- shoppingList.js
+++ shoppingList.js
@@ -1,18 +1,52 @@
-//returns the list
-{
-    "_id": "5d9c7b9ee2d16f00178a5c6b",
-    "userId": "<userid>",
-    "name": "Einkaufen gehen",
-    "items": [
-        {
-            "bought": true,
-            "_id": "<imteid>",
-            "name": "Bananen"
-        },
-        {
-            "bought": false,
-            "_id": "<imteid>",
-            "name": "Äpfel"
-        }
-    ]
-}   
+
+//gibt Liste zurück
+function getlist() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("Items").innerHTML =
+        this.responseText;
+      }
+    };
+    xhttp.open("GET", "https://shopping-lists-api.herokuapp.com/api/v1/lists/5d9c7b9ee2d16f00178a5c6b", true);
+    xhttp.send();
+  }
+
+//add
+  function additem() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("Items").innerHTML =
+        this.responseText;
+      }
+    };
+    xhttp.open("POST", "https://shopping-lists-api.herokuapp.com/api/v1/lists/5d9c7b9ee2d16f00178a5c6b/items", true);
+    xhttp.send();
+  }
+
+//remove item
+function removeitem() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("Items").innerHTML =
+        this.responseText;
+      }
+    };
+    xhttp.open("DELETE", "https://shopping-lists-api.herokuapp.com/api/v1/lists/5d9c7b9ee2d16f00178a5c6b/items/ITEMID???", true);
+    xhttp.send();
+  }
+
+  //update item
+  function updateitem() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("Items").innerHTML =
+        this.responseText;
+      }
+    };
+    xhttp.open("PUT", "https://shopping-lists-api.herokuapp.com/api/v1/lists/5d9c7b9ee2d16f00178a5c6b/items/ITEMID???", true);
+    xhttp.send();
+  }
