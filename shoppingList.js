0e99050f0910875c3eb852d1ca21036284b13b07
Tobias Strauss <54841030+tobistrauss@users.noreply.github.com>
2019-10-29 14:07:14 +0100

    Add files via upload

--- shoppingList.js
+++ shoppingList.js
@@ -13,11 +13,11 @@ function getlist(id) {
 }
 
 //add
-function additem() {
+function additem(input) {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("Items").innerHTML =
+      document.getElementById(input).innerHTML =
       this.responseText;
     }
   };
