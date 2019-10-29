6d25051b3c5a6a11d86fcbc854bfa69805a684f0
Tobias Strauss <54841030+tobistrauss@users.noreply.github.com>
2019-10-29 14:11:44 +0100

    Add files via upload

--- shoppingList.js
+++ shoppingList.js
@@ -76,7 +76,7 @@ document.addEventListener('change', function (e) {
 var input = document.getElementById("myInput");
 
 // Execute a function when the user releases a key on the keyboard
-input.addEventListener("keyup", function(event) {
+input.addEventListener("keydown", function(event) {
   // Number 13 is the "Enter" key on the keyboard
   if (event.keyCode === 13) {
     // Cancel the default action, if needed
