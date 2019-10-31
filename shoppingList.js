ecb8b7e377a0da4b90b31a4b529a2b1fe5a3ab36
Tobias Strauss <54841030+tobistrauss@users.noreply.github.com>
2019-10-31 18:52:09 +0100

    Add files via upload

--- shoppingList.js
+++ shoppingList.js
@@ -1,11 +1,13 @@
+var input = document.getElementById("myInput");
 
 //gibt Liste zurück
 function getlist(id) {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("Items").innerHTML =
-      this.responseText;
+
+      var antwort = JSON.parse(this.responseText);
+      document.getElementById("Items").innerHTML = antwort._id; //.name //.items
     }
   };
   xhttp.open("GET", "https://shopping-lists-api.herokuapp.com/api/v1/lists/" + id, true);
@@ -13,17 +15,20 @@ function getlist(id) {
 }
 
 //add
-function additem(input) {
+function additem(id) {
   var xhttp = new XMLHttpRequest();
+  var jsonObject={"itemName" : input};
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      document.getElementById(input).innerHTML =
-      this.responseText;
+      document.getElementById("Items").innerHTML =
+      JSON.parse(this.responseText);
     }
   };
   xhttp.open("POST", "https://shopping-lists-api.herokuapp.com/api/v1/lists/"+id+"/items", true);
-  xhttp.send();
+  xhttp.send(JSON.stringify(jsonObject));
 }
+
+
 
 //remove item
 function removeitem() {
@@ -75,16 +80,13 @@ document.addEventListener('change', function (e) {
 // Get the input field
 var input = document.getElementById("myInput");
 
-// Execute a function when the user releases a key on the keyboard
+// Execute a function when the user presses a key on the keyboard
 input.addEventListener("keydown", function(event) {
   // Number 13 is the "Enter" key on the keyboard
-  if (event.keyCode === 13) {
+  if (e.which == 13 || event.keyCode == 13) {
     // Cancel the default action, if needed
     event.preventDefault();
     // Trigger the button element with a click
     document.getElementById("myBtn").click();
   }
 });
-
-
-
