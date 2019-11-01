e1b23aca205d3c6d98c42c193d0d00552a37a50a
Tobias Strauss <54841030+tobistrauss@users.noreply.github.com>
2019-11-01 16:36:45 +0100

    Add files via upload

---
+++ requests.js
@@ -0,0 +1,101 @@
+//gibt Liste zurück
+
+var serverUrl = "https://shopping-lists-api.herokuapp.com";
+var apiKey = "69847ae2d173f7ea582485a659a88883";
+
+/*
+GET /api/v1/lists
+Authorization: <API Key>
+*/
+function getAllLists() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+  
+        var lists = JSON.parse(this.responseText);
+        showItemLists(lists);
+        // document.getElementById("Items").innerHTML = antwort._id; //.name //.items
+      }
+    };
+    xhttp.open("GET", serverUrl + "/api/v1/lists/", true);
+    xhttp.setRequestHeader('Authorization', apiKey);
+    xhttp.send();
+}
+
+
+function getList(id) {
+    storeListId(id);
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+  
+        var list = JSON.parse(this.responseText);
+        addItemList(list);
+        // document.getElementById("Items").innerHTML = antwort._id; //.name //.items
+      }
+    };
+    xhttp.open("GET", serverUrl + "/api/v1/lists/" + id, true);
+    xhttp.send();
+}
+ 
+//add
+/*
+POST /api/v1/lists/<listid>/items
+Content-Type: application/json
+
+{ "name": "" }
+*/
+function addItem(value) {
+    var id = loadListId();
+    var xhttp = new XMLHttpRequest();
+    var jsonObject= {"name" : value };
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        var list = JSON.parse(this.responseText);
+        addItemList(list);
+      }
+    };
+    xhttp.open("POST", serverUrl + "/api/v1/lists/"+id+"/items", true);
+    xhttp.setRequestHeader('Content-type', 'application/json');
+    xhttp.send(JSON.stringify(jsonObject));
+  }
+  
+//remove item
+function removeItem(id) {
+    var listId = loadListId();
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        var list = JSON.parse(this.responseText);
+        addItemList(list);
+      }
+    };
+    xhttp.open("DELETE", serverUrl + "/api/v1/lists/"+listId+"/items/"+id , true);
+    xhttp.send();
+  }
+
+  
+//update item
+function updateItem() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("Items").innerHTML =
+        this.responseText;
+      }
+    };
+    xhttp.open("PUT", serverUrl + "/api/v1/lists/"+id+"/items/ITEMID???", true);
+    xhttp.send();
+  }
+  
+  
+function storeListId(id) {
+    localStorage.setItem('ListId', id);
+  }
+  
+  function loadListId() {
+    return localStorage.getItem('ListId');
+  }
+
+  getAllLists();
+  
--- shoppingList.js
+++ shoppingList.js
@@ -1,67 +1,10 @@
 var input = document.getElementById("myInput");
 
-//gibt Liste zurück
-function getlist(id) {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-
-      var antwort = JSON.parse(this.responseText);
-      document.getElementById("Items").innerHTML = antwort._id; //.name //.items
-    }
-  };
-  xhttp.open("GET", "https://shopping-lists-api.herokuapp.com/api/v1/lists/" + id, true);
-  xhttp.send();
-}
-
-//add
-function additem(id) {
-  var xhttp = new XMLHttpRequest();
-  var jsonObject={"itemName" : input};
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("Items").innerHTML =
-      JSON.parse(this.responseText);
-    }
-  };
-  xhttp.open("POST", "https://shopping-lists-api.herokuapp.com/api/v1/lists/"+id+"/items", true);
-  xhttp.send(JSON.stringify(jsonObject));
-}
-
-
-
-//remove item
-function removeitem() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("Items").innerHTML =
-      this.responseText;
-    }
-  };
-  xhttp.open("DELETE", "https://shopping-lists-api.herokuapp.com/api/v1/lists/"+id+"/items/ITEMID???", true);
-  xhttp.send();
-}
-
-//update item
-function updateitem() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("Items").innerHTML =
-      this.responseText;
-    }
-  };
-  xhttp.open("PUT", "https://shopping-lists-api.herokuapp.com/api/v1/lists/"+id+"/items/ITEMID???", true);
-  xhttp.send();
-}
-
-
 
 document.getElementById("listSelect").addEventListener('change', function(event) {
-console.log(event.target.value); // Get ID of list
-getlist(event.target.value);
-})
+  console.log(event.target.value); // Get ID of list
+  getList(event.target.value);
+});
 
 
 //Checkbox
@@ -70,12 +13,42 @@ document.addEventListener('change', function (e) {
     if (e.target.name === 'checkItems') {
       if (e.target.checked) {
         e.target.parentNode.className = 'selected';
+        setTimeout(function() {
+          removeItem(e.target.id);
+        }, 3000);
       } else {
         e.target.parentNode.className = '';
       }
     }
 });
 
+function showItemLists(lists) {
+  // 
+  var id = loadListId();
+  var select = document.getElementById("listSelect");
+  var html = '';
+  lists.forEach(list => {
+    if (!id) {
+      id = list._id;
+      storeListId(id);
+    }
+    var selected = id === list._id ? true : false;
+    if (selected) {
+      addItemList(list);
+    }
+    html += '<option selected="'+selected+'" value="'+list._id+'">'+list.name+'</option> ';
+  });
+  select.innerHTML = html;
+}
+
+function addItemList(list) {
+  var div = document.getElementById("item-list");
+  var html  = "";
+  list.items.forEach(item => {
+    html +=  '<label><input name="checkItems" id="'+item._id+'" type="checkbox" /><span class="checkbox-ghost"> </span>'+item.name+'</label>';
+  });
+  div.innerHTML = html;
+}
 
 // Get the input field
 var input = document.getElementById("myInput");
@@ -83,10 +56,11 @@ var input = document.getElementById("myInput");
 // Execute a function when the user presses a key on the keyboard
 input.addEventListener("keydown", function(event) {
   // Number 13 is the "Enter" key on the keyboard
-  if (e.which == 13 || event.keyCode == 13) {
+  if (event.which == 13 || event.keyCode == 13) {
     // Cancel the default action, if needed
     event.preventDefault();
+    addItem(input.value);
     // Trigger the button element with a click
-    document.getElementById("myBtn").click();
+//    document.getElementById("myBtn").click();
   }
 });
